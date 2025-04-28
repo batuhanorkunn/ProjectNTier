9eb779dff1d64dc1e6e7e3ed187d8c5ca250e8fa
batuhanorkunn <batuhanorkunn@gmail.com>
2025-04-28 22:33:32 +0300

    Add project files.

---
+++ Project.ENTITIES/Models/AppUserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+	public class AppUserProfile : BaseEntity
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Phone { get; set; }
+		public string Address { get; set; }
+
+		//Relational Properties
+		public virtual AppUser AppUser { get; set; }
+	}
+}
